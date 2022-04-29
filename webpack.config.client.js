f912e567167a226c4e8b1ba08a7431810337d2db
Stephen Moss <bladexr8@gmail.com>
2022-04-29 15:09:43 +1000

    revert UI

--- webpack.config.client.js
+++ webpack.config.client.js
@@ -23,10 +23,6 @@ const config = {
                 use: [
                     'babel-loader'
                 ]
-            },
-            {
-                test: /\.(ttf|eot|svg|gif|jpg|png)(\?[\s\S]+)?$/,
-                use: 'file-loader'
             }
         ]
     },  
--- webpack.config.client.production.js
+++ webpack.config.client.production.js
@@ -19,10 +19,6 @@ const config = {
                 use: [
                     'babel-loader'
                 ]
-            },
-            {
-                test: /\.(ttf|eot|svg|gif|jpg|png)(\?[\s\S]+)?$/,
-                use: 'file-loader'
             }
         ]
     }
