25f58fe8aca98d5b967399df0d40d584ab8ac3ee
Stephen Moss <bladexr8@gmail.com>
2022-04-29 15:15:28 +1000

    revert UI

--- webpack.config.client.production.js
+++ webpack.config.client.production.js
@@ -1,27 +1,28 @@
 const path = require('path')
+const webpack = require('webpack')
 const CURRENT_WORKING_DIR = process.cwd()
 
 const config = {
     mode: "production",
     entry: [
-        path.join(CURRENT_WORKING_DIR, 'client/main.js')
+      path.join(CURRENT_WORKING_DIR, 'client/main.js')
     ],
     output: {
-        path: path.join(CURRENT_WORKING_DIR , '/dist'),
-        filename: 'bundle.js',
-        publicPath: "/dist/"
+      path: path.join(CURRENT_WORKING_DIR , '/dist'),
+      filename: 'bundle.js',
+      publicPath: "/dist/"
     },
     module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                use: [
-                    'babel-loader'
-                ]
-            }
-        ]
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: [
+            'babel-loader'
+          ]
+        }
+      ]
     }
-}
+  }
 
 module.exports = config
